350a8dd2b9465e4f4190eaaa24c7e324bcd73663
David Třetina <tretinadavid@gmail.com>
2024-01-07 01:15:48 +0100

    move theme to separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,8 @@ import Navigation from '@/components/navigation';
 import UploadBtn from '@/components/uploadBtn';
 import ImageGrid from '@/components/imageGrid';
 
-import { ThemeOptions } from '@mui/material/styles';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
-import createTheme from '@mui/material/styles/createTheme';
-
-import { createClient } from '@supabase/supabase-js'
-import { useEffect } from 'react';
-
-export const themeOptions: ThemeOptions = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#6b7557',
-    },
-    secondary: {
-      main: '#f50057',
-    },
-  },
-  typography: {
-    h1: {
-      fontFamily: 'Sacramento',
-    }
-  },
-});
+import { themeOptions } from '@/theme';
 
 export default function Home() {
   return (
