b82de71c4a3db513c4ce0cd7e1d70954265f991b
David Třetina <tretinadavid@gmail.com>
2024-01-07 01:10:22 +0100

    image loading from supabase

--- src/components/imageGrid.tsx
+++ src/components/imageGrid.tsx
@@ -1,34 +1,44 @@
 import React, { useEffect, useState } from "react"
 import { ImageList, ImageListItem } from "@mui/material"
+import { supabase } from "@/lib/api";
+import Image from "next/image";
 
 export default function ImageGrid() {
     const [images, setImages] = useState([]);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        // const fetchImages = async () => {
-        //     const listRef = ref(storage, '');
-
-        //     try {
-        //         const res = await listAll(listRef);
-        //         const imageUrls: any = await Promise.all(res.items.map(item => getDownloadURL(item)));
-        //         setImages(imageUrls);
-        //     } catch (error) {
-        //         // Handle error
-        //     }
-        // }
-
-        // fetchImages();
+        (async () => {
+            const { data, error }: any = await supabase.storage.from('images').list('', {
+                limit: 20
+            });
+            if (error) {
+                setError(error);
+                console.error(error);
+            } else {
+                const urls: any = data.map((image: any) => supabase.storage.from('images').getPublicUrl(image.name).data.publicUrl);
+                setImages(urls);
+            }
+        })();
     }, []);
 
+    if (error) {
+        return <div>Error: {(error as any).message}</div>;
+    }
+
     return (
-        <ImageList cols={3} gap={8} rowHeight={130}>
-            {Array.isArray(images) && images.map((url: string, index: number) => (
+        <ImageList cols={3} gap={8} rowHeight={200}>
+            {Array.isArray(images) && images.map((url, index) => (
                 <ImageListItem key={index}>
-                    <img
-                        srcSet={url}
+                    <Image
                         src={url}
-                        alt={'photo'}
-                        loading="lazy"
+                        quality={50}
+                        alt={`photo ${index}`}
+                        priority
+                        fill
+                        style={{
+                            objectFit: 'cover', // cover, contain, none
+                        }}
                     />
                 </ImageListItem>
             ))}
