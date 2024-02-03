51361b6c1a9aad8af1359e3fec9d3483590bb939
David Třetina <tretinadavid@gmail.com>
2024-02-03 22:03:19 +0100

    Delete unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,9 @@ import ImageGrid from './_components/ImageGrid';
 import { Loader } from './_components/Loader';
 
 export default async function Home() {
-  //   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <main>
-      {/* <ShowcaseDialog
-        openedImage={state.showcaseOpen}
-        handleClose={() => dispatch(toggleShowcase(null))}
-      /> */}
-
-      {/* <ImageGrid
-        images={state.images}
-        addImage={(image: ImageType) => dispatch(addImage(image))}
-        showcaseOpen={(image: ImageType | null) =>
-          dispatch(toggleShowcase(image))
-        }
-      /> */}
-
       {/* TODO: Add loading skeleton (not necessary)*/}
       <Suspense fallback={<Loader />}>
         <ImageGrid />
