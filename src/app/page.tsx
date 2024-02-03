cd14d645454ff864a5fbf23bbc0421db31af409b
David Třetina <tretinadavid@gmail.com>
2024-02-04 00:51:56 +0100

    feat: add skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { Suspense } from 'react';
 import ImageGrid from './_components/ImageGrid';
-import { Loader } from './_components/Loader';
+import Loader from './_components/Loader';
 
 export default async function Home() {
 
   return (
     <main>
-      {/* TODO: Add loading skeleton (not necessary)*/}
       <Suspense fallback={<Loader />}>
         <ImageGrid />
       </Suspense>
