d38af0932612b07f5385c7db137e7ccf242d624c
David Třetina <tretinadavid@gmail.com>
2024-02-03 21:38:01 +0100

    fix: not using reducer

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,18 +1,7 @@
 'use server';
 import { signIn } from '@/auth';
-import { Action } from './reducer';
 import { ImageType } from './types';
 import { AuthError } from 'next-auth';
-
-export const addImage = (image: ImageType): Action => ({
-  type: 'add',
-  image,
-});
-
-export const toggleShowcase = (showcaseOpen: ImageType | null): Action => ({
-  type: 'toggleShowcase',
-  showcaseOpen,
-});
 
 export async function authenticate(
   prevState: string | undefined,
